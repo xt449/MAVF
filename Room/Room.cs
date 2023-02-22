0da21c6fa37a643efb9f3c0bfa47c2c8e2e5566b
Jonathan Talcott <jetalcott@milcorp.com>
2023-02-22 13:00:17 -0500

    Basic room object system

---
+++ Room/Room.cs
@@ -0,0 +1,30 @@
+﻿using MILAV.API.Device;
+using System.Collections.Generic;
+
+namespace MILAV.Room
+{
+    public class Room
+    {
+        private readonly string id;
+        private readonly string name;
+
+        // Only CONTROL type devices
+        private readonly Dictionary<string, IDevice> devices = new Dictionary<string, IDevice>();
+
+        public Room(string id, string name, IEnumerable<IDevice> devices)
+        {
+            this.id = id;
+            this.name = name;
+
+            foreach(var device in devices)
+            {
+                this.devices[device.Id] = device;
+            };
+        }
+
+        public bool TryGetDevice(string id, out IDevice device)
+        {
+            return devices.TryGetValue(id, out device);
+        }
+    }
+}
---
+++ Room/RoomManager.cs
@@ -0,0 +1,27 @@
+﻿using MILAV.API.Device;
+using System;
+using System.Collections.Generic;
+
+namespace MILAV.Room
+{
+    public class RoomManager
+    {
+        private readonly Dictionary<string, Room> rooms = new Dictionary<string, Room>();
+
+        public RoomManager()
+        {
+        }
+
+        public Room AddRoom(string id, string name, IEnumerable<IDevice> devices)
+        {
+            if(rooms.ContainsKey(id)) {
+                return null;
+            }
+
+            var room = new Room(id, name, devices);
+            rooms[id] = room;
+
+            return room;
+        }
+    }
+}
