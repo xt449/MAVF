5b5d491d4ec199967bd16c14789db70ce901785d
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-11 09:26:55 -0400

    Moved device implementations to MILAV from MILAV API. created device categories for TVTuner, VideoController, VideoWall, and VTC

---
+++ Device/VTC/CustomVTC.cs
@@ -0,0 +1,29 @@
+﻿using MILAV.API.Device;
+using MILAV.API.Device.TVTuner;
+
+namespace MILAV.Device.VTC
+{
+    [Device("customvtc")]
+    public class CustomVTC : AbstractDevice, IChannelControl, IPowerControl
+    {
+        public string GetChannel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetChannel(string channel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetPower()
+        {
+            return false;
+        }
+
+        public void SetPower(bool state)
+        {
+            // nothing
+        }
+    }
+}
