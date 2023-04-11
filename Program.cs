dcc1e7570e32e671f813fdcf41e594bab22cbbfe
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-11 13:49:24 -0400

    Switch to SignalR

--- Program.cs
+++ Program.cs
@@ -4,27 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 
 // Initialize real backend
 var controller = new Controller();
-// Link backend
+// Inject backend
 builder.Services.AddScoped(_ => controller);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 //app.UseAuthorization();
 
-app.MapControllers();
+app.MapHub<SignalRHub>("");
 
 app.Run();
---
+++ SignalRHub.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using MILAV.API.Connection;
+using MILAV.API.Device;
+
+namespace MILAV
+{
+    public class SignalRHub : Hub
+    {
+        protected readonly Controller controller;
+
+        public SignalRHub(Controller controller)
+        {
+            this.controller = controller;
+        }
+
+        public AbstractDevice[] GetDevices()
+        {
+            return controller.GetDevices();
+        }
+
+        public AbstractDevice? GetDeviceById(string id)
+        {
+            controller.TryGetDevice(id, out AbstractDevice? device);
+            return device;
+        }
+
+        public string? GetDeviceDriverById(string id)
+        {
+            controller.TryGetDevice(id, out AbstractDevice? device);
+            return device?.Driver;
+        }
+
+        public string? GetDeviceIpById(string id)
+        {
+            controller.TryGetDevice(id, out AbstractDevice? device);
+            return device?.Ip;
+        }
+
+        public int? GetDevicePortById(string id)
+        {
+            controller.TryGetDevice(id, out AbstractDevice? device);
+            return device?.Port;
+        }
+
+        public Protocol? GetDeviceProtocolById(string id)
+        {
+            controller.TryGetDevice(id, out AbstractDevice? device);
+            return device?.Protocol;
+        }
+
+        public string? GetDeviceRoomById(string id)
+        {
+            controller.TryGetDevice(id, out AbstractDevice? device);
+            return device?.Room;
+        }
+
+        public ControlState? GetDeviceStateById(string id)
+        {
+            controller.TryGetDevice(id, out AbstractDevice? device);
+            return device?.State;
+        }
+
+        public ControlState[]? GetDeviceStatesById(string id)
+        {
+            controller.TryGetDevice(id, out AbstractDevice? device);
+            return device?.States;
+        }
+
+        public string[]? GetDeviceStateRoomsById(string id, string state)
+        {
+            controller.TryGetDevice(id, out AbstractDevice? device);
+            return device?.States.FirstOrDefault(s => s.Id == state)?.Rooms;
+        }
+    }
+}
