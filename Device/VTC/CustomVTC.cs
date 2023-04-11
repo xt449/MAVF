2cdc3d2c7760ad92494ed45eb67283d2c926507c
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-11 10:48:08 -0400

    Implemented basic VTC functionality

--- Device/VTC/CustomVTC.cs
+++ Device/VTC/CustomVTC.cs
@@ -1,29 +1,83 @@
 ﻿using MILAV.API.Device;
 using MILAV.API.Device.TVTuner;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace MILAV.Device.VTC
 {
     [Device("customvtc")]
     public class CustomVTC : AbstractDevice, IChannelControl, IPowerControl
     {
-        public string GetChannel()
+        [JsonProperty("requestGetChannel")]
+        public string GetChannelRequest { get; }
+
+        [JsonProperty("responseGetChannel")]
+        public string GetChannelResponse { get; }
+
+        [JsonProperty("requestSetChannel")]
+        public string SetChannelRequest { get; }
+
+        [JsonProperty("requestGetPower")]
+        public string GetPowerRequest { get; }
+
+        [JsonProperty("responseGetPower")]
+        public string GetPowerResponse { get; }
+
+        [JsonProperty("requestSetPower")]
+        public string SetPowerRequest { get; }
+
+        public string? GetChannel()
         {
-            throw new NotImplementedException();
+            if (Connection.Connect())
+            {
+                Connection.WriteASCII(GetChannelRequest);
+
+                var match = Regex.Match(Connection.ReadASCII(), GetChannelResponse);
+                if (match.Success)
+                {
+                    return match.Value;
+                }
+            }
+
+            return null;
         }
 
         public void SetChannel(string channel)
         {
-            throw new NotImplementedException();
+            if (Connection.Connect())
+            {
+                // RegEx formatting ($1)?
+                // C# formatting ({0})?
+                // or something else?
+                Connection.WriteASCII(SetChannelRequest.Replace("$1", channel));
+            }
         }
 
         public bool GetPower()
         {
+            if (Connection.Connect())
+            {
+                Connection.WriteASCII(GetPowerRequest);
+
+                var match = Regex.Match(Connection.ReadASCII(), GetPowerResponse);
+                if (match.Success)
+                {
+                    return bool.Parse(match.Value);
+                }
+            }
+
             return false;
         }
 
         public void SetPower(bool state)
         {
-            // nothing
+            if (Connection.Connect())
+            {
+                // RegEx formatting ($1)?
+                // C# formatting ({0})?
+                // or something else?
+                Connection.WriteASCII(SetPowerRequest.Replace("$1", state ? "1" : "0"));
+            }
         }
     }
 }
