a213b2e261c947b15afc04b5949c102b395a0bc5
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-14 15:39:24 -0400

    Starting NVXController implementation

---
+++ Device/NVX/NVXController.cs
@@ -0,0 +1,30 @@
+﻿using MILAV.API;
+using MILAV.API.Device;
+using MILAV.API.Device.Routing;
+using Newtonsoft.Json;
+
+namespace MILAV.Device.NVX
+{
+    public class NVXController : IDevice, IRouteControl<NVXEndpoint, NVXEndpoint>
+    {
+        public string Id { get; init; }
+
+        [JsonConverter(typeof(IdentifiableCollectionToDictionaryConverter<InputOutputPort>))]
+        [JsonProperty(Required = Required.DisallowNull)]
+        public Dictionary<string, NVXEndpoint> Inputs { get; init; }
+
+        [JsonConverter(typeof(IdentifiableCollectionToDictionaryConverter<InputOutputPort>))]
+        [JsonProperty(Required = Required.DisallowNull)]
+        public Dictionary<string, NVXEndpoint> Outputs { get; init; }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IRouteControl<NVXEndpoint, NVXEndpoint>.ExecuteRoute(NVXEndpoint input, NVXEndpoint output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Device/NVX/NVXEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using MILAV.API.Device.Routing;
+using Newtonsoft.Json;
+
+namespace MILAV.Device.NVX
+{
+    public class NVXEndpoint : IInputOutput
+    {
+        public string Id { get; init; }
+
+        public IOType Type { get; init; }
+
+        public string Group { get; init; }
+
+        [JsonProperty(Required = Required.DisallowNull)]
+        public readonly string ip;
+    }
+}
