ab168174d6c0d4f5de879da8f29ad1649d88e8c8
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-24 08:17:28 -0400

    Added CustomShadeController class

---
+++ Device/Shade/CustomShadeController.cs
@@ -0,0 +1,55 @@
+﻿using MILAV.API.Device;
+using MILAV.API.Device.Shade;
+using MILAV.API.Device.TVTuner;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+namespace MILAV.Device.TVTuner
+{
+    [Device("customtshade")]
+    public class CustomShadeController : AbstractNetworkDevice, IShadeControl
+    {
+        [JsonProperty(Required = Required.DisallowNull)]
+        public readonly string requestShadesClose;
+
+        [JsonProperty(Required = Required.DisallowNull)]
+        public readonly string responseShadesHalf;
+
+        [JsonProperty(Required = Required.DisallowNull)]
+        public readonly string requestShadesOpen;
+        
+        public void ShadesClose()
+        {
+            if (Connection.Connect())
+            {
+                // RegEx formatting ($1)?
+                // C# formatting ({0})?
+                // or something else?
+                Connection.WriteASCII(requestShadesClose);
+            }
+        }
+
+        public void ShadesHalf()
+        {
+            if (Connection.Connect())
+            {
+                // RegEx formatting ($1)?
+                // C# formatting ({0})?
+                // or something else?
+                Connection.WriteASCII(responseShadesHalf);
+            }
+        }
+
+        public void ShadesOpen()
+        {
+            if (Connection.Connect())
+            {
+                // RegEx formatting ($1)?
+                // C# formatting ({0})?
+                // or something else?
+                Connection.WriteASCII(requestShadesOpen);
+            }
+        }
+    }
+}
