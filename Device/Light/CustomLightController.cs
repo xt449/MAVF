5bc9e1c5381953e0337396f2e3bbe2bd0c03743d
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-24 08:48:11 -0400

    Add CustomLightController class

---
+++ Device/Light/CustomLightController.cs
@@ -0,0 +1,23 @@
+﻿using MILAV.API.Device;
+using MILAV.API.Device.Light;
+using Newtonsoft.Json;
+
+namespace MILAV.Device.TVTuner
+{
+    public class CustomLightController : AbstractNetworkDevice, ILightControl
+    {
+        [JsonProperty(Required = Required.DisallowNull)]
+        public readonly string requestSetLightLevel;
+
+        public void SetLightLevel(float lightLevel)
+        {
+            if (Connection.Connect())
+            {
+                // RegEx formatting ($1)?
+                // C# formatting ({0})?
+                // or something else?
+                Connection.WriteASCII(requestSetLightLevel.Replace("$1", lightLevel.ToString()));
+            }
+        }
+    }
+}
