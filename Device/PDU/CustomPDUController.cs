a49fb0780cb6785ec4845f466e02fddcb3bdd8ca
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-24 08:14:47 -0400

    Added CustomPDUController class

---
+++ Device/PDU/CustomPDUController.cs
@@ -0,0 +1,38 @@
+﻿using MILAV.API.Device;
+using MILAV.API.Device.PDU;
+using Newtonsoft.Json;
+
+namespace MILAV.Device.USB
+{
+    [Device("custompdu")]
+    public class CustomPDUController : AbstractNetworkDevice, IPDUControl
+    {
+        [JsonProperty(Required = Required.DisallowNull)]
+        public readonly string requestTurnOffPower;
+
+        [JsonProperty(Required = Required.DisallowNull)]
+        public readonly string requestTurnOnPower;
+
+        public void TurnPowerOff(int port)
+        {
+            if (Connection.Connect())
+            {
+                // RegEx formatting ($1)?
+                // C# formatting ({0})?
+                // or something else?
+                Connection.WriteASCII(requestTurnOffPower.Replace("$1", port.ToString()));
+            }
+        }
+
+        public void TurnPowerOn(int port)
+        {
+            if (Connection.Connect())
+            {
+                // RegEx formatting ($1)?
+                // C# formatting ({0})?
+                // or something else?
+                Connection.WriteASCII(requestTurnOnPower.Replace("$1", port.ToString()));
+            }
+        }
+    }
+}
